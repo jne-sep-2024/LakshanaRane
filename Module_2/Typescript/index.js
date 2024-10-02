d6617914caed06831e84a7efa103024b3406aee3
LakshanaRane <lakshana.rane@thinkitive.com>
2024-10-02 12:56:37 +0530

    1)routings 2)reacthookspractice 3)authentication 4)axiosreact 5)counter app  4)typescript(started)

---
+++ Module_2/Typescript/index.js
@@ -0,0 +1,7 @@
+var age = 23;
+if (age < 50) {
+    age += 10;
+}
+console.log(age);
+var greetings = "hello laksh";
+console.log(greetings);
---
+++ Module_2/Typescript/objects.ts
@@ -0,0 +1,13 @@
+const user={
+    name: "lakshana",
+    email:"laksh@mail.com",
+    isActive: true
+}
+
+function createUser(name: string,isPaid:boolean){
+
+}
+// createUser({name:"laksh",isPaid:true}); 
+
+
+// createUser():{}{}  syntax
