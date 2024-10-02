66f44f9951685af0ef8bc591404758e50be23063
LakshanaRane <lakshana.rane@thinkitive.com>
2024-10-02 12:10:53 +0530

    Removed node_modules from tracking

--- Module_2/19_Hooks_Practice/react-hooks/usestate/src/App.jsx
+++ Module_2/19_Hooks_Practice/react-hooks/usestate/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useEffect, useState } from "react";
 
 import "./App.css";
 
@@ -7,14 +7,29 @@ function App() {
 
   // const inc= ()=>{setCount(count+1)};
   // const dec= ()=>{setCount(count-1)};
+  console.log("lakshana");
+
+  let s1=setTimeout(() => {
+    console.log("hello");
+
+  }, 5000);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(
+        s1,
+        console.log("bye")
+      );
+    };
+  }, []);
 
   return (
     <>
-    <button onClick={()=>setCount(count+1)}>+</button>
-    <button onClick={()=>setCount(count-1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
       <p>The count is {count}</p>
     </>
   );
 }
 
-export default App;
+export default memo(App);
